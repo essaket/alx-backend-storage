0c7df790aaef1cc2ab1c47b598cd69ce3d9706de
essaket <hichamessaket17@gmail.com>
2024-08-14 14:08:07 -0700

    Writing task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,39 +1,37 @@
+
 #!/usr/bin/env python3
-"""Module containing function to return HTML content of a particular URL"""
+""" Tracker callls """
+
 import redis
 import requests
+from typing import Callable
 from functools import wraps
 
-data = redis.Redis()
+r = redis.Redis()
 
 
-def cached_content_fun(method):
-    """Function that returns html content"""
+def count_calls(method: Callable) -> Callable:
+    """ Decorator to know the number of calls """
 
     @wraps(method)
-    def wrapper(url: str):
-        cached_content = data.get(f"cached:{url}")
-        if cached_content:
-            return cached_content.decode('utf-8')
+    def wrapper(url):
+        """ Wrapper decorator """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
-        content = method(url)
-        data.setex(f"cached:{url}", 10, content)
-        return content
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
 
-@cached_content_fun
+@count_calls
 def get_page(url: str) -> str:
-    """Function thattracks how many times a particular URL was accessed"""
+    """ Get page
+    """
+    req = requests.get(url)
+    return req.text
 
-    count = data.incr(f"count:{url}")
-    content = requests.get(url).text
-    # print(content)
-    # print("Count: {}".format(count))
-    return content
-
-
-# if __name__ == "__main__":
-    # get_page('http://slowwly.robertomurray.co.uk')
-    # get_page('http://google.com')
