c1725889ed2cf910debe2babcdd564969258f8ca
essaket <hichamessaket17@gmail.com>
2024-08-14 17:56:32 -0700

    Update README.md

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,40 +1,36 @@
 #!/usr/bin/env python3
-""" A caching request module
 """
-import redis
+web cache and tracker
+"""
 import requests
+import redis
 from functools import wraps
-from typing  import Callable, Any
+
+store = redis.Redis()
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ getter for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """
-        wrapper that checks if a url's data is cached
-        and tracks how many times the get_page is called.
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@track_get_page
+@count_url_access
 def get_page(url: str) -> str:
-    """
-    Makes a http request to a given endpoint
-    params:
-        url - the url of the web page to retreive
-    Returns:
-        The content of the web page.
-    """
-    response = requests.get(url)
-    return response.text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
